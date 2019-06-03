983c85dc80701900000cb66707360bb913ee06fe
michalf2 <51335724+michalf2@users.noreply.github.com>
2019-06-03 19:39:36 +0200

    Piąty commit

--- 01program.php
+++ 01program.php
@@ -3,7 +3,8 @@ echo("Witaj");
 echo ("<br> Jestem programem 01");
 echo ("<br> Test");
 echo ("<br> Test2");
-echo ("<br> Dalsza część programu")
-echo ("<br> Jeszcze dalsza część programu")  
+echo ("<br> Dalsza część programu");
+echo ("<br> Jeszcze dalsza część programu") ; 
+echo ("<br> Lubie placki");  
 ?>
 
