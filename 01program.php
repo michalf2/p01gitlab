5fb5baba4187dafd71511eeea543f1d87e7bc6b9
michalf2 <51335724+michalf2@users.noreply.github.com>
2019-06-03 19:38:37 +0200

    Update 01program.php

--- 01program.php
+++ 01program.php
@@ -3,6 +3,7 @@ echo("Witaj");
 echo ("<br> Jestem programem 01");
 echo ("<br> Test");
 echo ("<br> Test2");
-echo ("<br? Dalsza część programu")
+echo ("<br> Dalsza część programu")
+echo ("<br> Jeszcze dalsza część programu")  
 ?>
 
